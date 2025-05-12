44e9706435b1903e8d4ad377361da3f95bcd63ef
Ddeepakgoutam2005 <160123712+Ddeepakgoutam2005@users.noreply.github.com>
2025-05-13 00:11:40 +0530

    Update loginAPI.js

--- frontend/src/store/api/loginAPI.js
+++ frontend/src/store/api/loginAPI.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const loginAPI = createApi({
   reducerPath: "loginAPI",
   baseQuery: fetchBaseQuery({
-    baseUrl: "process.env.REACT_APP_API_BASE_URL",
+    baseUrl: "REACT_APP_API_BASE_URL",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().userInfoReducer.tokenId;
       if (token) headers.set("Authorization", `Bearer ${token}`);
